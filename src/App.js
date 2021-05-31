df1dd389e6fdb8eecfe29ecf30d08f841bda0a73
JCDMeira <jean.meira10@hotmail.com>
2021-05-31 15:31:10 -0300

    Adiciona header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from "react";
+
+// # Importação de páginas
 import Login from "./pages/login";
+
+// # Importação de componentes
+import Header from "../src/components/header";
+
+// # Importação de estilos
 import GlobalStyle from "./styles/globalStyles";
 
 function App() {
   return (
     <>
+      <Header />
       <GlobalStyle />
       <Login />
     </>
