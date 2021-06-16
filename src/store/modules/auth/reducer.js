25b0652398ebd5061f35b5812dbd62924052c670
JCDMeira <jean.meira10@hotmail.com>
2021-06-15 21:37:24 -0300

    Corrige erro de token de usuário  ativo após logout

--- src/store/modules/auth/reducer.js
+++ src/store/modules/auth/reducer.js
@@ -1,4 +1,5 @@
 import * as types from "../types";
+import axios from "../../../services/axios";
 
 const initialState = {
   isLoggedIn: false,
@@ -14,6 +15,7 @@ export default function (state = initialState, action) {
       return newState;
     }
     case types.LOGIN_FAILURE: {
+      delete axios.defaults.headers.Authorization;
       const newState = { ...initialState };
       return newState;
     }
