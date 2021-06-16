e6cbbc778ef19e3e084ceefe1b5c569fa660b076
JCDMeira <jean.meira10@hotmail.com>
2021-06-15 21:35:05 -0300

    Adiciona função de excluir alunos para usuários logados

--- src/pages/Alunos/index.js
+++ src/pages/Alunos/index.js
@@ -2,13 +2,19 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "../../services/axios";
 import { get } from "lodash";
-import { FaUserCircle, FaEdit, FaWindowClose } from "react-icons/fa";
+import {
+  FaUserCircle,
+  FaEdit,
+  FaWindowClose,
+  FaExclamation,
+} from "react-icons/fa";
+import { toast } from "react-toastify";
 
-// # Importação de estilos
+// # Styles
 import { Container } from "../../styles/globalStyles";
 import { AlunoContainer, ProfilePicture } from "./styled";
 
-// # Arquivos próprios
+// # Own files
 //import * as exampleActions from "../../store/modules/example/actions";
 
 // # Components
@@ -28,14 +34,48 @@ export default function Alunos() {
 
     getData();
   }, []);
+
+  const handleDeleteAsk = (e) => {
+    e.preventDefault();
+    const exclamation = e.currentTarget.nextSibling;
+    exclamation.setAttribute("display", "block");
+    e.currentTarget.remove();
+    toast.error("clique na exclamação para confirmar");
+  };
+
+  const handleDelete = async (e, id, index) => {
+    e.persist();
+
+    try {
+      setIsLoading(true);
+      await axios.delete(`/alunos/${id}`);
+      const novosAlunos = [...alunos];
+      novosAlunos.splice(index, 1);
+      setAlunos(novosAlunos);
+      setIsLoading(false);
+    } catch (err) {
+      const status = get(err, "response.status", 0);
+
+      if (status === 401) {
+        toast.error("Você precisa fazer login");
+      } else {
+        toast.error("Ocorreu um erro ao excluir aluno");
+      }
+
+      setIsLoading(false);
+    }
+  };
+
   return (
     <Container>
       <Loading isLoading={isLoading} />
 
       <h1>Alunos</h1>
 
+      {/* <NovoAluno to="/aluno/">Novo aluno</NovoAluno> */}
+
       <AlunoContainer>
-        {alunos.map((aluno) => (
+        {alunos.map((aluno, index) => (
           <div key={String(aluno.id)}>
             <ProfilePicture>
               {get(aluno, "Fotos[0].url", false) ? (
@@ -44,6 +84,7 @@ export default function Alunos() {
                 <FaUserCircle size={36} />
               )}
             </ProfilePicture>
+
             <span>{aluno.nome}</span>
             <span>{aluno.email}</span>
 
@@ -51,9 +92,16 @@ export default function Alunos() {
               <FaEdit size={16} />
             </Link>
 
-            <Link to={`/aluno/${aluno.id}/delete`}>
+            <Link onClick={handleDeleteAsk} to={`/aluno/${aluno.id}/delete`}>
               <FaWindowClose size={16} />
             </Link>
+
+            <FaExclamation
+              size={16}
+              display="none"
+              cursor="pointer"
+              onClick={(e) => handleDelete(e, aluno.id, index)}
+            />
           </div>
         ))}
       </AlunoContainer>
--- src/pages/Alunos/styled.js
+++ src/pages/Alunos/styled.js
@@ -1,12 +1,12 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 export const AlunoContainer = styled.div`
   margin-top: 20px;
-
   div {
     display: flex;
     align-items: center;
-    justify-content: center;
+    justify-content: space-between;
     padding: 5px 0;
   }
   div + div {
@@ -20,4 +20,9 @@ export const ProfilePicture = styled.div`
     height: 36px;
     border-radius: 50%;
   }
+`;
+
+export const NovoAluno = styled(Link)`
+  display: block;
+  padding: 20px 0 10px 0;
 `;
