39351aaa2bcf08c22e4bb6d1d64462ebc65da2d6
JCDMeira <jean.meira10@hotmail.com>
2021-06-18 23:03:29 -0300

    Adiciona campo de foto do aluno, em cadastro e edição de alunos

--- src/pages/Aluno/index.js
+++ src/pages/Aluno/index.js
@@ -5,9 +5,11 @@ import PropTypes from "prop-types";
 import { isEmail, isInt, isFloat } from "validator";
 import { toast } from "react-toastify";
 import { useDispatch } from "react-redux";
+import { FaEdit, FaUserCircle } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 // # Styles
-import { Form } from "./styled";
+import { Form, ProfilePicture, Title } from "./styled";
 import { Container } from "../../styles/globalStyles";
 
 // # Own files
@@ -19,13 +21,14 @@ import * as actions from "../../store/modules/auth/actions";
 export default function Aluno({ match }) {
   const dispach = useDispatch();
 
-  const id = get(match, "params.id", 0);
+  const id = get(match, "params.id", "");
   const [nome, setNome] = useState("");
   const [sobrenome, setSobrenome] = useState("");
   const [email, setEmail] = useState("");
   const [idade, setIdade] = useState("");
   const [peso, setPeso] = useState("");
   const [altura, setAltura] = useState("");
+  const [foto, setFoto] = useState("");
   const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
@@ -35,6 +38,8 @@ export default function Aluno({ match }) {
         setIsLoading(true);
         const { data } = await axios.get(`/alunos/${id}`);
         const Foto = get(data, "Fotos[0].url", "");
+
+        setFoto(Foto);
 
         setNome(data.nome);
         setSobrenome(data.sobrenome);
@@ -137,7 +142,16 @@ export default function Aluno({ match }) {
   return (
     <Container>
       <Loading isLoading={isLoading} />
-      <h1>{id ? "Editar aluno" : "Novo aluno"}</h1>
+      <Title>{id ? "Editar aluno" : "Novo aluno"}</Title>
+
+      {id && (
+        <ProfilePicture>
+          {foto ? <img src={foto} alt={nome} /> : <FaUserCircle size={180} />}
+          <Link to={`fotos/${id}`}>
+            <FaEdit size={24} />
+          </Link>
+        </ProfilePicture>
+      )}
 
       <Form onSubmit={handleSubmit}>
         <input
--- src/pages/Aluno/styled.js
+++ src/pages/Aluno/styled.js
@@ -1,5 +1,9 @@
 import styled from "styled-components";
+import * as colors from "../../config/colors";
 
+export const Title = styled.h1`
+  text-align: center;
+`;
 export const Form = styled.form`
   margin-top: 20px;
   display: flex;
@@ -11,5 +15,33 @@ export const Form = styled.form`
     border: 1px solid #ddd;
     border-radius: 4px;
     padding: 0 10px;
+  }
+`;
+export const ProfilePicture = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0 0 20px;
+  position: relative;
+  margin-top: 30px;
+
+  img {
+    width: 180px;
+    height: 180px;
+    border-radius: 50%;
+  }
+
+  a {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: none;
+    position: absolute;
+    bottom: 0;
+    color: #fff;
+    background: ${colors.primaryDarkColor};
+    width: 36px;
+    height: 36px;
+    border-radius: 50%;
   }
 `;
